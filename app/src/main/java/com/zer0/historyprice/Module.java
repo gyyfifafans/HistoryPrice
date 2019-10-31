e80dc6334b6b5c97c7ea64e92c6848a52d3f425c
kagura <xcehnz@163.com>
2019-10-31 22:39:15 +0800

    试修复

--- app/src/main/java/com/zer0/historyprice/Module.java
+++ app/src/main/java/com/zer0/historyprice/Module.java
@@ -7,7 +7,6 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 
 import java.lang.reflect.Field;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,7 +17,7 @@ import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
-public class Module implements IXposedHookLoadPackage{
+public class Module implements IXposedHookLoadPackage {
     private static final String PACKAGE_NAME = "com.jingdong.app.mall";
 
     private String shareUrl;
@@ -30,35 +29,24 @@ public class Module implements IXposedHookLoadPackage{
 
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        if (!lpparam.packageName.equals(PACKAGE_NAME)){
+        if (!lpparam.packageName.equals(PACKAGE_NAME)) {
             return;
         }
         LogUtil.log(lpparam.packageName);
 
-        XposedHelpers.findAndHookMethod(Class.class, "forName",
-                String.class, boolean.class, ClassLoader.class, new XC_MethodHook() {
+
+        XposedHelpers.findAndHookMethod(ClassLoader.class, "loadClass", String.class,
+                new XC_MethodHook() {
                     @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-//                        LogUtil.log("Class.forName: "+param.args[0]);
-                        if ("de.robv.android.xposed.XposedHelpers".equals(param.args[0])){
-                            LogUtil.log("Class.forName: " + param.args[0]);
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        if (param.hasThrowable()) return;
+                        Class<?> cls = (Class<?>) param.getResult();
+                        if (cls != null && cls.getName().contains(XPOSED_BRIDGE)) {
+                            LogUtil.log("try to load " + XPOSED_BRIDGE);
                             param.setResult(null);
                         }
                     }
                 });
-
-
-        XposedHelpers.findAndHookMethod(ClassLoader.class, "loadClass", String.class, new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                if (param.hasThrowable()) return;
-                Class<?> cls = (Class<?>) param.getResult();
-                if (cls != null && cls.getName().contains(XPOSED_BRIDGE)) {
-                    LogUtil.log("try to load " + XPOSED_BRIDGE);
-                    param.setResult(null);
-                }
-            }
-        });
 
 
         XposedHelpers.findAndHookMethod("com.jd.stat.security.jma.JMA",
@@ -72,49 +60,52 @@ public class Module implements IXposedHookLoadPackage{
 
 
         XposedHelpers.findAndHookMethod(Activity.class,
-                "findViewById", int.class, new XC_MethodHook(){
+                "findViewById", int.class, new XC_MethodHook() {
                     @Override
                     protected void afterHookedMethod(final MethodHookParam param) throws Throwable {
                         Activity activity = (Activity) param.thisObject;
 //                        LogUtil.log("activity: " + activity);
                         if (!activity.toString().contains(SHARE_ACTIVITY_NAME) ||
-                                XposedHelpers.getAdditionalInstanceField(activity, btnAdded) != null){
+                                XposedHelpers.getAdditionalInstanceField(activity, btnAdded) != null) {
                             return;
                         }
                         shareActivity = activity;
                         LogUtil.log("shareActivity:  " + shareActivity);
+                        if (GridView.class.isAssignableFrom(param.getResult().getClass())){
+                            GridView view = (GridView) param.getResult();
 
-                        View view = (View) param.getResult();
-                        if (view.getClass().toString().contains("JDGridView")){
-                            Field list = XposedHelpers.findFirstFieldByExactType(activity.getClass(), List.class);
-                            list.setAccessible(true);
-                            try {
-                                // 添加按钮
-                                List<Map<String, Object>> shareChannels = (List) list.get(param.thisObject);
-                                Map<String, Object> lastView = shareChannels.get(shareChannels.size() - 1);
-                                LogUtil.log("lastView: " + lastView);
+                            LogUtil.log("get View: " + view);
+                            if (view.getClass().toString().contains("JDGridView")) {
+                                Field list = XposedHelpers.findFirstFieldByExactType(activity.getClass(), List.class);
+                                list.setAccessible(true);
+                                try {
+                                    // 添加按钮
+                                    List<Map<String, Object>> shareChannels = (List) list.get(activity);
+                                    Map<String, Object> lastView = shareChannels.get(shareChannels.size() - 1);
+                                    LogUtil.log("lastView: " + lastView);
 
-                                Map<String, Object> map = new HashMap<>();
-                                map.put("text", "其他");
-                                map.put("channel", "Other");
-                                map.put("image", lastView.get("image"));
-                                shareChannels.add(map);
-                                XposedHelpers.setAdditionalInstanceField(activity, btnAdded, true);
-                                LogUtil.log("added share button");
+                                    Map<String, Object> map = new HashMap<>();
+                                    map.put("text", "其他");
+                                    map.put("channel", "Other");
+                                    map.put("image", lastView.get("image"));
+                                    shareChannels.add(map);
+                                    XposedHelpers.setAdditionalInstanceField(activity, btnAdded, true);
+                                    LogUtil.log("added share button");
 
 
-                                // 获取分享链接
-                                Class clazz = XposedHelpers.findClassIfExists(
-                                        "com.jingdong.common.entity.ShareInfo", lpparam.classLoader);
-                                Field field = XposedHelpers.findFirstFieldByExactType(activity.getClass() ,clazz);
-                                field.setAccessible(true);
-                                shareUrl = (String) XposedHelpers.callMethod(
-                                        field.get(activity), "getUrl");;
-                                LogUtil.log("shareUrl:" + shareUrl);
+                                    // 获取分享链接
+                                    Class clazz = XposedHelpers.findClassIfExists(
+                                            "com.jingdong.common.entity.ShareInfo", lpparam.classLoader);
+                                    Field field = XposedHelpers.findFirstFieldByExactType(activity.getClass(), clazz);
+                                    field.setAccessible(true);
+                                    shareUrl = (String) XposedHelpers.callMethod(
+                                            field.get(activity), "getUrl");
+                                    LogUtil.log("shareUrl:" + shareUrl);
 
-                            }catch (Exception e){
-                                LogUtil.log("JD shareActivity error: " + e.getMessage());
-                                e.printStackTrace();
+                                } catch (Exception e) {
+                                    LogUtil.log("JD shareActivity error: " + e.getMessage());
+                                    e.printStackTrace();
+                                }
                             }
 
                         }
@@ -125,29 +116,38 @@ public class Module implements IXposedHookLoadPackage{
                 AdapterView.OnItemClickListener.class, new XC_MethodHook() {
                     @Override
                     protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        LogUtil.log("item clicked: " + lpparam.classLoader + ", " + Arrays.toString(param.args));
-                        Object itemClickListener = param.args[0];
-                        LogUtil.log("ItemClickListener class: " + itemClickListener.getClass().getName());
-                        XposedHelpers.findAndHookMethod(itemClickListener.getClass(), "onItemClick",
-                                AdapterView.class, View.class, int.class, long.class, new XC_MethodHook() {
-                                    @Override
-                                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                                        if (XposedHelpers.getAdditionalInstanceField(shareActivity, clicked) != null){
-                                            return;
-                                        }
+                        LogUtil.log("ItemClickListener.shareActivity: " + shareActivity);
+                        if (shareActivity != null) {
+//                        LogUtil.log("item clicked: " + lpparam.classLoader + ", " + Arrays.toString(param.args));
+                            Object itemClickListener = param.args[0];
+                            LogUtil.log("ItemClickListener.class: " + itemClickListener.getClass().getName());
 
-                                        GridView gridView = (GridView) param.args[0];
-                                        int position = (int) param.args[2];
-                                        if (position == gridView.getAdapter().getCount() - 1){
-                                            XposedHelpers.setAdditionalInstanceField(shareActivity, clicked, true);
-                                            Intent shareIntent = new Intent();
-                                            shareIntent.setAction(Intent.ACTION_SEND);
-                                            shareIntent.putExtra(Intent.EXTRA_TEXT, shareUrl);
-                                            shareIntent.setType("text/plain");
-                                            shareActivity.startActivity(shareIntent);
-                                        }
-                                    }
-                                });
+                            if (itemClickListener.getClass().getName().startsWith("com.jingdong.app.mall.basic")) {
+                                XposedHelpers.findAndHookMethod(itemClickListener.getClass(), "onItemClick",
+                                        AdapterView.class, View.class, int.class, long.class, new XC_MethodHook() {
+                                            @Override
+                                            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                                                if (XposedHelpers.getAdditionalInstanceField(shareActivity, clicked) != null) {
+                                                    return;
+                                                }
+                                                LogUtil.log("onItemClick: " + param.thisObject);
+
+                                                GridView gridView = (GridView) param.args[0];
+                                                int position = (int) param.args[2];
+                                                LogUtil.log("onItemClick.position: " + position);
+                                                if (position == gridView.getAdapter().getCount() - 1) {
+                                                    XposedHelpers.setAdditionalInstanceField(shareActivity, clicked, true);
+                                                    Intent shareIntent = new Intent();
+                                                    shareIntent.setAction(Intent.ACTION_SEND);
+                                                    shareIntent.putExtra(Intent.EXTRA_TEXT, shareUrl);
+                                                    shareIntent.setType("text/plain");
+                                                    shareActivity.startActivity(shareIntent);
+                                                }
+                                            }
+                                        });
+                            }
+
+                        }
                     }
                 });
 
